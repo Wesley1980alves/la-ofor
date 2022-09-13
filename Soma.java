c0af479e861e379f24c48269fe888659e8d3ca41
wesleyalves <110191958+Wesley1980alves@users.noreply.github.com>
2022-09-13 17:09:42 -0300

    Create Soma.java
    intendendo como funciona o laco for

---
+++ Soma.java
@@ -0,0 +1,16 @@
+package somavalores;
+
+public class Soma {
+
+	public static void main(String[] args) {
+		int total = 0;
+		for(int conta=2;conta<=20;conta+=2) {
+			total+=conta;
+
+		}
+		System.out.printf("resultado %d\n",total);
+		
+
+	}
+
+}
